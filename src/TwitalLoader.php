eeca2c5a6bbb0675c0509658d99275311c38a5cc
porl <porl42@gmail.com>
2020-01-04 23:26:29 +1100

    testing getsourcecontext issue

--- src/TwitalLoader.php
+++ src/TwitalLoader.php
@@ -60,6 +60,7 @@ class TwitalLoader implements \Twig\Loader\LoaderInterface
     public function getSourceContext($name)
     {
 	/* TODO: PORL!!!! */
+        die($this->loader->getSourceContext($name));
         return $this->loader->getSourceContext($name);
         $originalContext = $this->loader->getSourceContext($name);
         $code = $originalContext->getCode();
